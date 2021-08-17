14ba6ae927f91c5eac1b71e00c9a716c83bac58f
kurtkungz <kurtkungz@gmail.com>
2021-08-17 23:49:09 +0700

    add margin-bottom on breadcrumbs

--- single.php
+++ single.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 <section class="main-content flex flex-col md:flex-row md:divide-x md:divide-gray-300">
     <section class="md:w-3/4 prose max-w-none md:pr-5">
-		<?php
-		if ( function_exists( 'aioseo_breadcrumbs' ) ) {
-			aioseo_breadcrumbs();
-		} ?>
+		<?php if ( function_exists( 'aioseo_breadcrumbs' ) ) : ?>
+            <section class="mb-10">
+				<?php aioseo_breadcrumbs(); ?>
+            </section>
+		<?php endif; ?>
 
 		<?php the_content(); ?>
 		<?php edit_post_link(); ?>
