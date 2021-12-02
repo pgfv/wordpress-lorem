69a01b651d56a039c9e25b852a839f095f923297
kurtkungz <kurtkungz@gmail.com>
2021-12-02 23:39:00 +0700

    present modified time in single.php

--- category.php
+++ category.php
@@ -1,30 +1,30 @@
 <?php get_header(); ?>
 <section class="content-container" role="main">
-    <section class="main-content">
-		<?php if ( function_exists( 'aioseo_breadcrumbs' ) ) : ?>
-            <section class="mb-10">
-				<?php aioseo_breadcrumbs(); ?>
-            </section>
-		<?php endif; ?>
+  <section class="main-content">
+    <?php if (function_exists('aioseo_breadcrumbs')) : ?>
+      <section class="mb-10">
+        <?php aioseo_breadcrumbs(); ?>
+      </section>
+    <?php endif; ?>
 
-        <h1><?php single_cat_title( 'Category: ' ); ?></h1>
-		<?php if ( have_posts() ): ?>
-            <section class="grid grid-cols-2 md:grid-cols-3 gap-5">
-				<?php while ( have_posts() ): the_post(); ?>
-                    <article class="article-box">
-                        <a href="<?php the_permalink(); ?>">
-							<?php the_post_thumbnail(); ?>
-							<?php the_title(); ?>
-                        </a>
-                    </article>
-				<?php endwhile; ?>
-            </section>
-		<?php else: ?>
-            <p>Sorry, no posts matched your criteria.</p>
-		<?php endif; ?>
-    </section>
-    <aside class="main-aside">
-		<?php dynamic_sidebar( 'primary' ); ?>
-    </aside>
+    <h1><?php single_cat_title('Category: '); ?></h1>
+    <?php if (have_posts()) : ?>
+      <section class="grid grid-cols-2 md:grid-cols-3 gap-5">
+        <?php while (have_posts()) : the_post(); ?>
+          <article class="article-box">
+            <a href="<?php the_permalink(); ?>">
+              <?php the_post_thumbnail(); ?>
+              <?php the_title(); ?>
+            </a>
+          </article>
+        <?php endwhile; ?>
+      </section>
+    <?php else : ?>
+      <p>Sorry, no posts matched your criteria.</p>
+    <?php endif; ?>
+  </section>
+  <aside class="main-aside">
+    <?php dynamic_sidebar('primary'); ?>
+  </aside>
 </section>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,27 +1,33 @@
 <?php get_header(); ?>
 <section class="content-container" role="main">
-    <section class="main-content">
-		<?php if ( function_exists( 'aioseo_breadcrumbs' ) ) : ?>
-            <section class="mb-10">
-				<?php aioseo_breadcrumbs(); ?>
-            </section>
-		<?php endif; ?>
+  <section class="main-content">
+    <?php if (function_exists('aioseo_breadcrumbs')) : ?>
+      <section class="mb-10">
+        <?php aioseo_breadcrumbs(); ?>
+      </section>
+    <?php endif; ?>
 
-		<?php the_content(); ?>
+    <?php the_content(); ?>
 
-        <address class="flex flex-row space-x-3 mt-5">
-            <div>Author: <?php the_author_meta( 'display_name', 1 ); ?></div>
-            <div>Date: <?php echo get_the_date(); ?></div>
-        </address>
+    <address class="flex flex-col space-y-3 mt-5 text-sm uppercase">
+      <div>Published: <time><?php echo get_the_date('F j, Y'); ?></time> By <?php the_author_meta('display_name', 1); ?></div>
+      <?php
+      $u_time = get_the_time( 'U' );
+      $u_modified_time = get_the_modified_time( 'U' );
+      
+      if ( $u_modified_time >= $u_time + 86400 ) : ?>
+      <div>Last modified on <time><?php the_modified_time( 'F j, Y' ); ?></time></div>
+      <?php endif; ?>
+    </address>
 
-        <nav class="flex flex-row justify-between mt-5">
-            <div><?php previous_post_link( '&laquo; %link' ); ?></div>
-            <div><?php next_post_link( '%link &raquo;' ); ?></div>
-        </nav>
-		<?php edit_post_link(); ?>
-    </section>
-    <aside class="main-aside">
-		<?php dynamic_sidebar( 'primary' ); ?>
-    </aside>
+    <nav class="flex flex-row justify-between mt-5">
+      <div><?php previous_post_link('&laquo; %link'); ?></div>
+      <div><?php next_post_link('%link &raquo;'); ?></div>
+    </nav>
+    <?php edit_post_link(); ?>
+  </section>
+  <aside class="main-aside">
+    <?php dynamic_sidebar('primary'); ?>
+  </aside>
 </section>
 <?php get_footer(); ?>
